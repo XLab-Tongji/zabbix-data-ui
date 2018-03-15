72732bc581caa762f3f40fe487f7c4e8003f6bd8
Rain <iheyu22@163.com>
2018-03-15 19:49:22 +0800

    init vue framework

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="HelloWorld">
+    <div class="wrapper wrapper-content animated fadeInRight">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="text-center m-t-lg">
+            <h1>
+              Welcome in INSPINIA Static SeedProject
+            </h1>
+            <small>
+              It is an application skeleton for a typical web app. You can use it to quickly bootstrap your webapp projects and dev environment for these projects.
+            </small>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
